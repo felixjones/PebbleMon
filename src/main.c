719ef85eb4f13d0e9185fa541929a7a9ca5107b6
Felix Jones <fewix3000@hotmail.co.uk>
2014-08-17 21:24:12 +0100

    Fixed crash

--- src/main.c
+++ src/main.c
@@ -9,18 +9,17 @@ int main() {
 	Memory_Initialise( &__heap, sizeof( __heap ) );
 	APP_LOG( APP_LOG_LEVEL_INFO, "Initialised memory" );
 	
-	if ( SceneManager_Scene() == NULLPTR ) {
-		APP_LOG( APP_LOG_LEVEL_INFO, "Starting base scene" );
-		xiSceneBase_t * const sceneBase = SceneBase_Init( SceneBase_Alloc() );
-		if ( sceneBase ) {
-			SceneManager_Call( sceneBase );
-			Object_Release( sceneBase );
-		}
+	APP_LOG( APP_LOG_LEVEL_INFO, "Starting base scene" );
+	xiSceneBase_t * const sceneBase = SceneBase_Init( SceneBase_Alloc() );
+	if ( sceneBase ) {
+		SceneManager_Call( sceneBase );
+		Object_Release( sceneBase );
 	}
 
 	app_event_loop();
 	
 	APP_LOG( APP_LOG_LEVEL_INFO, "Shutting down" );
+	SceneManager_Exit();
 
 	return 0;
 }
