90dd6f003068deb5055a550c1d0d978f784b03f8
Felix Jones <fewix3000@hotmail.co.uk>
2014-08-17 21:15:58 +0100

    New timer

--- src/main.c
+++ src/main.c
@@ -3,14 +3,15 @@
 #include "SceneManager.h"
 #include "SceneBase.h"
 
-static uint8_t __heap[0x0400];
+static uint8_t		__heap[0x0400];
+static AppTimer *	updateTimer = NULLPTR;
 
-void Update( struct tm * tick_time, TimeUnits units_changed ) {
+void Update( void * userPtr ) {
 	if ( SceneManager_Scene() == NULLPTR ) {
 		APP_LOG( APP_LOG_LEVEL_INFO, "Starting base scene" );
 		xiSceneBase_t * const sceneBase = SceneBase_Init( SceneBase_Alloc() );
 		if ( sceneBase ) {
-			SceneManager_Goto( sceneBase );
+			SceneManager_Call( sceneBase );
 			Object_Release( sceneBase );
 		}
 	}
@@ -18,17 +19,18 @@ void Update( struct tm * tick_time, TimeUnits units_changed ) {
 	APP_LOG( APP_LOG_LEVEL_INFO, "Update called" );
 
 	SceneManager_Run();
+
+	app_timer_cancel( updateTimer );
+	updateTimer = app_timer_register( 500, &Update, NULLPTR );
 }
 
 int main() {
 	Memory_Initialise( &__heap, sizeof( __heap ) );
 	APP_LOG( APP_LOG_LEVEL_INFO, "Initialised memory" );
 
-	tick_timer_service_subscribe( SECOND_UNIT, &Update );
+	updateTimer = app_timer_register( 500, &Update, NULLPTR );
 
 	app_event_loop();
-	
-	tick_timer_service_unsubscribe();
 	
 	APP_LOG( APP_LOG_LEVEL_INFO, "Shutting down" );
 
